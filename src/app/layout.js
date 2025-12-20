716fa829d00503d74252d1a10deed82dc34777ac
Rajat Rajdeep <46029666+RajatRajdeep@users.noreply.github.com>
2025-12-20 17:20:26 +0530

    add google-site-verification meta for SEO google verification (#33)

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ export { metadata };
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <meta name="google-site-verification" content="Z4hozcIBBrLFjoeT038NmGotLZIkhtLRTMF8aJIAosQ" />
+      </head>
       <body className="flex flex-col min-h-screen bg-background-light dark:bg-background-dark text-gray-900 dark:text-gray-100">
         <ThemeProvider>
           <Header themeToggle={<ThemeToggle />} />
