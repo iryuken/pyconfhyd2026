a538ee39ac836aa7f26d2019b4c105f929f5a751
Rajat Rajdeep <46029666+RajatRajdeep@users.noreply.github.com>
2026-01-09 22:06:31 +0530

    enable dark mode konfhub widget (#40)

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -13,21 +13,21 @@ export default function Tickets() {
         </MdxLayout>
       </div>
       <iframe
-        className="flex"
+        className="flex dark:hidden"
         src="https://konfhub.com/widget/pyconf-hyderabad-2026?desc=false&secondaryBg=FDD100&ticketBg=FFF9DC&borderCl=FDD100&bg=FFFFFF&fontColor=1e1f24&ticketCl=1e1f24&btnColor=FB5850&fontFamily=Nunito&borderRadius=10&widget_type=standard"
         id="konfhub-widget"
         title="Register for PyConf Hyderabad 2026"
         width="100%"
         height="800"
       ></iframe>
-      {/* <iframe
+      <iframe
         className="hidden dark:flex"
-        src="https://konfhub.com/widget/pyconf-hyderabad-2026?desc=false&secondaryBg=C07600&ticketBg=C07600&borderCl=C07600&bg=1A202C&fontColor=F9FAFB&ticketCl=FFF&btnColor=000&fontFamily=Nunito&borderRadius=10&widget_type=standard"
+        src="https://konfhub.com/widget/pyconf-hyderabad-2026?desc=false&secondaryBg=C07600&ticketBg=C07600&borderCl=C07600&bg=1A202C&fontColor=F9FAFB&ticketCl=FFF&btnColor=000&anchorTag=fff&fontFamily=Nunito&borderRadius=10&widget_type=standard"
         id="konfhub-widget"
         title="Register for PyConf Hyderabad 2026"
         width="100%"
         height="800"
-      ></iframe> */}
+      ></iframe>
     </section>
   );
 }
