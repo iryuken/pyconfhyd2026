0a16ef27fdde7a65b619b959b947ce14fec28523
Joanna Calamus <joannacalamus@gmail.com>
2026-02-05 03:22:05 +0530

    align keynote speakers section design with the current theme (#52)
    * Fix issue #38
    
    * More fixes for issue #38
    
    * Address review comments for issue #38
    
    * issue #38 speaker
    
    * speaker title change per review comment
    
    ---------
    
    Co-authored-by: Joanna <joannacalamus123@gmail.com>

--- src/components/KeynoteSpeakers.jsx
+++ src/components/KeynoteSpeakers.jsx
@@ -5,16 +5,16 @@ export default function KeynoteSpeakers({ speakers }) {
   return (
     <section
       id="keynote-speakers"
-      className="scroll-mt-20 flex flex-col items-center py-6 w-11/12 lg:w-5/6 mx-auto"
+      className="relative z-0 scroll-mt-20 flex flex-col items-center py-6 w-11/12 lg:w-5/6 mx-auto"
     >
       <Heading
         tagLevel={2}
         level={1}
-        className="text-center my-8 text-primary-600 dark:text-primary-400"
+        className="text-center my-2 text-primary-600 dark:text-primary-400"
       >
         Keynote Speakers
       </Heading>
-      <div className="flex flex-wrap justify-center mt-10 gap-20">
+      <div className="flex flex-wrap justify-center gap-20">
         {speakers.map((speaker, index) => (
           <Speaker key={index} speaker={speaker} />
         ))}
